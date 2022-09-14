2f613f9f605ab1663f91c32f6ce7a7edb164595b
Andrew Heyer <andrewbheyer@gmail.com>
2022-09-13 23:20:54 -0700

    updating allows hosts

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
